deb7aca685a6bdaca23bd1ab5c159593b57644c8
Aravind175-cell <avulaaravind00789@gmail.com>
2025-02-26 11:39:06 +0530

    third commit

--- src/Formik.js
+++ src/Formik.js
@@ -30,7 +30,7 @@ const Formik = () => {
  
   return (
     <div className="APP">
-      {/* <h1>-----Formik</h1> */}
+      {/* <h1>-----Formik</h1> */} 
       <form autoComplete="off" onSubmit={formik.handleSubmit}>  {/* Turned off autocomplete for the entire form */}
         <label>Name</label> <br/>
         <input 
@@ -72,6 +72,7 @@ const Formik = () => {
         <br/>
         <br/>
         <button type="submit">Register</button>
+        {/*example */}
       </form>
     </div>
   )
